df0925df7533528583f93989bb260085a6dd6af5
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2024-02-28 16:19:54 +0100

    Only allow 100 grid-size maximum

--- script.js
+++ script.js
@@ -10,23 +10,25 @@ const button = document.querySelector('button')
 button.addEventListener('click', getSize)
 
 function getSize(){
-  const inputSize = prompt("How big o' grid?")
-  console.log(inputSize)
-  setGrid();
-  
-  function setGrid(size){
-    size = inputSize
-    canvas.style.setProperty('--grid-size', size)
-    canvas.innerHTML = ''
-    for (let i = 0; i < (size * size); i++) {
-      const pixel = document.createElement('div')
-      pixel.style.borderStyle = "solid"
-      pixel.style.height = "100%"
-      pixel.style.width = "100%"
-      canvas.appendChild(pixel);
-      pixel.addEventListener("mouseover", setBackground)
+  const inputSize = prompt("How big o' grid? (Max = 100)")
+  if (inputSize > 100) alert("That's too huge dude...")
+  else {
+    console.log(inputSize)
+    setGrid();
+    
+    function setGrid(size){
+      size = inputSize
+      canvas.style.setProperty('--grid-size', size)
+      canvas.innerHTML = ''
+      for (let i = 0; i < (size * size); i++) {
+        const pixel = document.createElement('div')
+        pixel.style.borderStyle = "solid"
+        pixel.style.borderColor = "lightgrey"
+        pixel.style.height = "100%"
+        pixel.style.width = "100%"
+        canvas.appendChild(pixel);
+        pixel.addEventListener("mouseover", setBackground)
+      }
     }
   }
 }
-
-
